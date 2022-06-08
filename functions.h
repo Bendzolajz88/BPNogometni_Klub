a108541afb773ae144794f3c4e7e70a5654fd49a
Benjamin <95294122+Bendzolajz88@users.noreply.github.com>
2022-06-08 10:08:46 +0200

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,31 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+typedef struct igrac {
+	int id;
+	char ime[25];
+	char prezime[30];
+	char pozicija[10];
+	int brreg;
+}IGRAC;
+
+int izbornik();
+int izbornikSrch();
+int izbornikSrt();
+void kreiranjeDat();
+void dodavanjeIgraca();
+void azuriranje();
+void* ucitavanjeIgraca();
+void ispisivanje(IGRAC*);
+void* SearchIme(IGRAC*);
+void* SearchPrezime(IGRAC*);
+void* SearchPozicija(IGRAC*);
+void* SearchBrReg(IGRAC*);
+void subs(IGRAC*, IGRAC*);
+void subs2(IGRAC*, IGRAC*);
+void selectionSortBrRegUzl(IGRAC*);
+void selectionSortBrRegSil(IGRAC*);
+void brisanjeIgr(IGRAC*);
+void brisanjeBaze(IGRAC*);
+
+#endif //FUNCTIONS_H
